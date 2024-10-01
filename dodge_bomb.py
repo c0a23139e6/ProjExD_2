7ac7d88b80c18e6dbf4f82e8a91ae4635b636024
proje <c0a23139e6@edu.teu.ac.jp>
2024-10-01 15:17:10 +0900

    練習４　衝突判定　

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -50,6 +50,9 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+        if kk_rct.colliderect(bb_rct):
+            print("GameOver")
+            return 
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]     # 横、縦
